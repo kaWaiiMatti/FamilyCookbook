41a66af9d1aec06bec1363d54ddad627270ae14b
Matti Pulkkinen <matti@pulkkinen.tv>
2024-10-09 21:26:49 +0300

    Setup backend proxy

--- FamilyCookbook.Client/vite.config.ts
+++ FamilyCookbook.Client/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/weatherforecast": "http://localhost:5263",
+      "/MicrosoftIdentity/": "http://localhost:5263",
+      "/signin-oidc": "http://localhost:5263",
+    },
+  },
 })
