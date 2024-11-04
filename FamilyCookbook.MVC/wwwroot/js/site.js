d8762c0029ff7a2944db4f1eeb7c2250c42da4d6
Matti Pulkkinen <matti@pulkkinen.tv>
2024-11-04 21:07:34 +0200

    Remove redundant js code

--- FamilyCookbook.MVC/wwwroot/js/site.js
+++ FamilyCookbook.MVC/wwwroot/js/site.js
@@ -1,10 +1,1 @@
-"use strict";
-const enableTableSort = () => {
-};
-Array
-    .from(document.getElementsByTagName("table"))
-    .filter(table => table.attributes.getNamedItem('data-sortable')?.value === 'true')
-    .forEach(table => {
-    console.log(table.id);
-});
-//# sourceMappingURL=site.js.map
+﻿
