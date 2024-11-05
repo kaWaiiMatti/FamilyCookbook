c8065ce5c6ee30bc268e0bed38e641262eb89278
Matti Pulkkinen <matti@pulkkinen.tv>
2024-11-05 13:28:08 +0200

    Revert "Add initial typescript support"
    This reverts commit 7cf6d9d0c40bc387760a80bbcfeaab46248a0e08.

--- FamilyCookbook.MVC/Scripts/src/test2.ts
+++
@@ -1,7 +0,0 @@
-const printStuff = (stuff: string): void => {
-  console.log(`hello ${stuff}`);
-}
-
-function helloClick(event: MouseEvent): void {
-  console.log(`hello ${(event.target as HTMLButtonElement).name}`);
-}
