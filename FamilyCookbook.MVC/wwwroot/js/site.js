00c1681c6b76eccc3aa7cab395761172b134c0be
Matti Pulkkinen <matti@pulkkinen.tv>
2024-11-05 13:26:49 +0200

    Revert "Remove redundant js code"
    This reverts commit d8762c0029ff7a2944db4f1eeb7c2250c42da4d6.

--- FamilyCookbook.MVC/wwwroot/js/site.js
+++ FamilyCookbook.MVC/wwwroot/js/site.js
@@ -1,1 +1,10 @@
-﻿
+"use strict";
+const enableTableSort = () => {
+};
+Array
+    .from(document.getElementsByTagName("table"))
+    .filter(table => table.attributes.getNamedItem('data-sortable')?.value === 'true')
+    .forEach(table => {
+    console.log(table.id);
+});
+//# sourceMappingURL=site.js.map
