bea9a1c1c6c00379fc0f368543cfeb7500070034
Matti Pulkkinen <matti@pulkkinen.tv>
2025-01-08 17:40:13 +0200

    Add Unit DataGrid

--- FamilyCookbook.App/src/views/UnitsView.tsx
+++ FamilyCookbook.App/src/views/UnitsView.tsx
@@ -1,5 +1,59 @@
-export const UnitsView = () => {
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { useEffect, useMemo, useState } from "react";
+import { Unit } from "../interfaces.ts";
+import { getUnits } from "../utils/apiClient.ts";
 
-  return <p>Units</p>;
+export const UnitsView = () => {
+  const [units, setUnits] = useState<Unit[]>([]);
+
+  useEffect(() => {
+    getUnits().then((result) => setUnits(result));
+  }, []);
+
+  const columns  = useMemo(() => [
+    {
+      header: 'ID',
+      accessorKey: 'id',
+    },
+    {
+      header: 'Name',
+      accessorKey: 'name',
+    },
+    {
+      header: 'Abbreviation',
+      accessorKey: 'abbreviation',
+    },
+  ], []);
+
+  const table = useReactTable({
+    columns,
+    data: units,
+    getCoreRowModel: getCoreRowModel()
+  });
+
+  const headers = table.getHeaderGroups()[0].headers;
+
+  return <table className='table is-striped is-fullwidth'>
+    <thead>
+      <tr>
+        {headers.map(header => (
+          <th key={header.id}>
+            {flexRender(header.column.columnDef.header, header.getContext())}
+          </th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+    {table.getRowModel().rows.map(row => (
+      <tr key={row.id}>
+        {row.getVisibleCells().map(cell => (
+          <td key={cell.id}>
+            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+          </td>
+        ))}
+      </tr>
+    ))}
+    </tbody>
+  </table>;
 };
 
