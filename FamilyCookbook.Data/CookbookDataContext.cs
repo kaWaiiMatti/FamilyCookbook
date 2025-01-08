019b6003d5ca936db3965a8730ef19ee7350b74f
Matti Pulkkinen <matti@pulkkinen.tv>
2025-01-08 19:22:21 +0200

    Fix FamilyMembers table name

--- FamilyCookbook.Data/CookbookDataContext.cs
+++ FamilyCookbook.Data/CookbookDataContext.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 public class CookbookDataContext(DbContextOptions<CookbookDataContext> options) : DbContext(options)
 {
     public DbSet<FamilyEntity> Families { get; set; }
+    public DbSet<FamilyMemberEntity> FamilyMembers { get; set; }
     public DbSet<IngredientEntity> Ingredients { get; set; }
     public DbSet<RecipeEntity> Recipes { get; set; }
     public DbSet<UnitEntity> Units { get; set; }
---
+++ FamilyCookbook.Data/Migrations/20250108172142_Fix FamilyMembers table name.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyCookbook.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixFamilyMemberstablename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FamilyMemberEntity_Families_FamilyId",
+                table: "FamilyMemberEntity");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FamilyMemberEntity",
+                table: "FamilyMemberEntity");
+
+            migrationBuilder.RenameTable(
+                name: "FamilyMemberEntity",
+                newName: "FamilyMembers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_FamilyMemberEntity_FamilyId",
+                table: "FamilyMembers",
+                newName: "IX_FamilyMembers_FamilyId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FamilyMembers",
+                table: "FamilyMembers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FamilyMembers_Families_FamilyId",
+                table: "FamilyMembers",
+                column: "FamilyId",
+                principalTable: "Families",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FamilyMembers_Families_FamilyId",
+                table: "FamilyMembers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FamilyMembers",
+                table: "FamilyMembers");
+
+            migrationBuilder.RenameTable(
+                name: "FamilyMembers",
+                newName: "FamilyMemberEntity");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_FamilyMembers_FamilyId",
+                table: "FamilyMemberEntity",
+                newName: "IX_FamilyMemberEntity_FamilyId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FamilyMemberEntity",
+                table: "FamilyMemberEntity",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FamilyMemberEntity_Families_FamilyId",
+                table: "FamilyMemberEntity",
+                column: "FamilyId",
+                principalTable: "Families",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
