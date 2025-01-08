99ecaca07aa19b013915a846754fe0ddddc486d9
Matti Pulkkinen <matti@pulkkinen.tv>
2025-01-08 17:11:37 +0200

    Remove msal usage from App.tsx

--- FamilyCookbook.App/src/App.tsx
+++ FamilyCookbook.App/src/App.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from "react";
 import { getUnits } from "./utils/apiClient";
-import { useMsal } from "@azure/msal-react";
 import { Unit } from "./interfaces.ts";
 
 function App() {
   const [result, setResult] = useState<Unit[] | null>(null);
 
-  const { accounts } = useMsal();
-
   useEffect(() => {
     getUnits().then((text) => setResult(text));
-  }, [accounts]);
+  }, []);
 
   return (
     <>
