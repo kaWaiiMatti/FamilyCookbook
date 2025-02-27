d8b18f0bc88571dae2a4d796394de6558626fea6
Matti Pulkkinen <matti@pulkkinen.tv>
2025-02-27 13:54:36 +0200

    Convert NavigationBar to bootstrap

--- FamilyCookbook.App/src/components/NavigationBar.tsx
+++ FamilyCookbook.App/src/components/NavigationBar.tsx
@@ -2,20 +2,18 @@ import { Link } from "react-router-dom";
 import { useMsal } from "@azure/msal-react";
 import { useEffect, useState } from "react";
 import {
+  Button,
   Collapse,
-  DropdownItem,
   DropdownMenu,
   DropdownToggle,
   Nav,
   Navbar,
-  NavbarText,
   NavbarToggler,
   NavItem,
-  NavLink,
   UncontrolledDropdown,
 } from "reactstrap";
 
-const NavigationBar = () => {
+export const NavigationBar = () => {
   const { instance } = useMsal();
 
   const [menuActive, setMenuActive] = useState<boolean>(false);
@@ -42,107 +40,47 @@ const NavigationBar = () => {
       <Collapse isOpen={menuActive} navbar>
         <Nav className="me-auto" navbar>
           <NavItem>
-            <NavLink href="/components/">Components</NavLink>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
           </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              GitHub
-            </NavLink>
-          </NavItem>
+
           <UncontrolledDropdown nav inNavbar>
             <DropdownToggle nav caret>
-              Options
+              Meals
             </DropdownToggle>
             <DropdownMenu right>
-              <DropdownItem>Option 1</DropdownItem>
-              <DropdownItem>Option 2</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem>
+              <Link className="dropdown-item" to="/new-meal">
+                New
+              </Link>
+              <Link className="dropdown-item" to="/meals">
+                Browse
+              </Link>
             </DropdownMenu>
           </UncontrolledDropdown>
+
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Recipes
+            </DropdownToggle>
+            <DropdownMenu right>
+              <Link className="dropdown-item" to="/new-recipe">
+                New
+              </Link>
+              <Link className="dropdown-item" to="/recipes">
+                Browse
+              </Link>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <NavItem>
+            <Link className="nav-link" to="/units">
+              Units
+            </Link>
+          </NavItem>
         </Nav>
-        <NavbarText>Simple Text</NavbarText>
+        <Button onClick={handleLogoutRedirect}>Sign out</Button>
       </Collapse>
     </Navbar>
   );
-
-  return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <button
-          onClick={() => setMenuActive((prevState) => !prevState)}
-          role="button"
-          className={`navbar-burger ${menuActive ? "is-active" : ""}`}
-          aria-label="menu"
-          aria-expanded="false"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </button>
-      </div>
-
-      <div className={`navbar-menu ${menuActive ? "is-active" : ""}`}>
-        <div className="navbar-start">
-          <Link className="navbar-item" to="/">
-            Home
-          </Link>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">Meals</a>
-
-            <div className="navbar-dropdown">
-              <Link className="navbar-item" to="/new-meal">
-                New
-              </Link>
-              <Link className="navbar-item" to="/meals">
-                Browse
-              </Link>
-            </div>
-          </div>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">Recipes</a>
-
-            <div className="navbar-dropdown">
-              <Link className="navbar-item" to="/new-recipe">
-                New
-              </Link>
-              <Link className="navbar-item" to="/recipes">
-                Browse
-              </Link>
-              <hr className="navbar-divider" />
-
-              <div className="navbar-item">
-                <p className="is-size-7 has-text-weight-bold">Featured</p>
-              </div>
-              <Link className="navbar-item" to="/events/event1">
-                Event 1
-              </Link>
-              <Link className="navbar-item" to="/events/event2">
-                Event 2
-              </Link>
-            </div>
-          </div>
-
-          <Link className="navbar-item" to="/units">
-            Units
-          </Link>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <button className="button" onClick={handleLogoutRedirect}>
-                Sign out
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
 };
-
-export { NavigationBar };
