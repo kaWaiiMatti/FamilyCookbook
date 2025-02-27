daadc2216dbe6eb17ff55ed1576675670530745a
Matti Pulkkinen <matti@pulkkinen.tv>
2025-02-27 13:19:58 +0200

    Expand navbar on sm size

--- FamilyCookbook.App/src/components/NavigationBar.tsx
+++ FamilyCookbook.App/src/components/NavigationBar.tsx
@@ -37,7 +37,7 @@ const NavigationBar = () => {
   };
 
   return (
-    <Navbar>
+    <Navbar expand="sm">
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="me-auto" navbar>
