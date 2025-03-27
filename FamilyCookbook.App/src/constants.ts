ce0c415a3d91eec8207bd9862e02fa5e4822feb1
Matti Pulkkinen <matti@pulkkinen.tv>
2025-03-27 21:40:00 +0200

    Renew adding new recipe

--- FamilyCookbook.App/src/constants.ts
+++ FamilyCookbook.App/src/constants.ts
@@ -1,2 +1,3 @@
+export const RECIPE_NAME_MAX_LENGTH = 100;
 export const UNIT_ABBREVIATION_MAX_LENGTH = 25;
 export const UNIT_NAME_MAX_LENGTH = 50;
--- FamilyCookbook.App/src/views/NewRecipeView.tsx
+++ FamilyCookbook.App/src/views/NewRecipeView.tsx
@@ -1,38 +1,34 @@
-import { SubmitHandler, useForm } from "react-hook-form";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { NewRecipeRequest } from "../interfaces.ts";
 import { createRecipe } from "../utils/apiClient.ts";
 import { useNavigate } from "react-router-dom";
 import { Button, Col, Row } from "reactstrap";
+import { Input } from "../components/FormComponents.tsx";
+import { RECIPE_NAME_MAX_LENGTH } from "../constants.ts";
+
+const formName = "new-recipe";
 
 export const NewRecipeView = () => {
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<NewRecipeRequest>();
+  const formMethods = useForm<NewRecipeRequest>();
+  const { handleSubmit } = formMethods;
   const onSubmit: SubmitHandler<NewRecipeRequest> = (data) => {
-    createRecipe(data).then((created) => {
-      console.log(created);
-      navigate("/recipes");
-    });
+    createRecipe(data).then(() => navigate("/recipes"));
   };
 
   return (
-    <>
+    <FormProvider {...formMethods}>
       <h1 className="title">New recipe</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="field">
-          <label className="label">Name</label>
-          <div className="control">
-            <input
-              className={`input ${errors.name ? "is-danger" : ""}`}
-              {...register("name", { required: true })}
-            />
-          </div>
-          {errors.name && (
-            <p className="help is-danger">This field is required</p>
-          )}
+        <div className="mb-3">
+          <Input
+            formName={formName}
+            label="Name"
+            maxLength={RECIPE_NAME_MAX_LENGTH}
+            name="name"
+            type="text"
+            required
+          />
         </div>
 
         <Row>
@@ -43,6 +39,6 @@ export const NewRecipeView = () => {
           </Col>
         </Row>
       </form>
-    </>
+    </FormProvider>
   );
 };
