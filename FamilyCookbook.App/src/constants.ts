bce59dea8f92eb8230754d7a3a982483dbbce31a
Matti Pulkkinen <matti@pulkkinen.tv>
2025-04-30 21:56:17 +0300

    Add meal related option lists

--- FamilyCookbook.App/src/constants.ts
+++ FamilyCookbook.App/src/constants.ts
@@ -1,3 +1,39 @@
+import { SelectOption } from "./components/FormComponents";
+
 export const RECIPE_NAME_MAX_LENGTH = 100;
 export const UNIT_ABBREVIATION_MAX_LENGTH = 25;
 export const UNIT_NAME_MAX_LENGTH = 50;
+
+export const MEAL_FRESHNESS_OPTIONS: SelectOption[] = [
+  { label: "Fresh made", value: "20" },
+  { label: "Fridge stored", value: "40" },
+  { label: "Freezer stored", value: "60" },
+];
+
+export const MEAL_TYPE_OPTIONS: SelectOption[] = [
+  {
+    label: "Breakfast",
+    value: "20",
+  },
+  {
+    label: "Lunch",
+    value: "40",
+  },
+  {
+    label: "Dinner",
+    value: "60",
+  },
+];
+
+export const MEAL_SOURCE_OPTIONS: SelectOption[] = [
+  { label: "Self-made", value: "20" },
+  { label: "Made by others", value: "40" },
+  { label: "Ready-made", value: "60" },
+  { label: "Restaurant (pick-up)", value: "80" },
+  { label: "Restaurant (dine-in)", value: "100" },
+];
+
+export const MEAL_STATUS_OPTIONS: SelectOption[] = [
+  { label: "Planned", value: "20" },
+  { label: "Realized", value: "40" },
+];
